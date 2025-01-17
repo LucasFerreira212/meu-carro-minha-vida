4613580d89775693d68c21aaa72f43564bf96f60
LucasFerreira212 <lucasrafa0190@gmail.com>
2025-01-17 14:36:26 -0300

    Alterações feitas

--- backend/src/main/java/com/MeuCarroMinhaVida/AppFinanciamento/AppFinanciamentoApplication.java
+++ backend/src/main/java/com/MeuCarroMinhaVida/AppFinanciamento/AppFinanciamentoApplication.java
@@ -2,12 +2,19 @@ package com.MeuCarroMinhaVida.AppFinanciamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppFinanciamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppFinanciamentoApplication.class, args);
 	}
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello from Spring Boot";
+	}
 }
