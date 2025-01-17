48ae5fb6fc5e0fb6e2aab024d2f3695402e48015
LucasFerreira212 <lucasrafa0190@gmail.com>
2025-01-17 09:20:57 -0300

    Commit inicial

---
+++ backend/src/main/java/com/MeuCarroMinhaVida/AppFinanciamento/AppFinanciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.MeuCarroMinhaVida.AppFinanciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFinanciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFinanciamentoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MeuCarroMinhaVida/AppFinanciamento/AppFinanciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeuCarroMinhaVida.AppFinanciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinanciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
